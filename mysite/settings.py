576bc58c99c36ee861f947a4774ea89ab0bec505
Domi <aigner_dominik@web.de>
2025-08-30 16:52:22 +0200

    fixed bug for sure this time

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
                         'localhost', 
                         '127.0.0.1',
-                        'mynamedomi.pythonanywhere.com/'
+                        'mynamedomi.pythonanywhere.com'
                         
                 ]
 
